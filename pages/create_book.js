d087fa56ee5e8915449025f2822b4a3f60b4f742
Xintong Zhang <zhang.xintong@northeastern.edu>
2024-04-01 20:03:40 -0400

    fixone of the potential vulnerabilities

--- pages/create_book.js
+++ pages/create_book.js
@@ -1,25 +1,27 @@
-let Book = require('../models/book');
-let Author = require('../models/author');
-let Genre = require('../models/genre');
+import db from '../db.js';
 
-function getAuthor(family_name, first_name) {
-  return Author.findOne({family_name: family_name, first_name: first_name});
+async function create(req, res) {
+  const { family_name, first_name, genre_name } = req;
+  
+  try {
+    const query1 = 'INSERT INTO genre (name) VALUES (?)';
+    const values1 = [genre_name];
+    const [genre_result] = await db.query(query1, values1);
+    const genre_id = genre_result.insertId;
+
+    const query2 = 'INSERT INTO book (family_name, first_name) VALUES (?, ?)';
+    const values2 = [family_name, first_name];
+    const [book_result] = await db.query(query2, values2);
+    const book_id = book_result.insertId;
+
+    const query3 = 'INSERT INTO book_genre (book, genre) VALUES (?, ?)';
+    const values3 = [book_id, genre_id];
+    await db.query(query3, values3);
+
+    res.redirect(`/book/${book_id}`);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 }
 
-function getGenre(name) {
-  return Genre.find({name: name});
-}
-
-exports.new_book = async (res, family_name, first_name, genre_name, title) => {
-  let author = await getAuthor(family_name, first_name).exec();
-  let genre = await getGenre(genre_name).exec();
-  let book = Book({
-    title: title,
-    summary: 'Demo Summary to be updated later',
-    author: author,
-    isbn: 'ISBN2022',
-    genre: genre
-  });
-  await book.save();
-  res.send('Created new book : ' + book);
-}
+export default create;
